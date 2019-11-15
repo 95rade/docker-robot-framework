6aaf6afbbe9348692722102a2044aee3215c0655
95rade <rade95@gmail.com>
2019-11-15 14:47:47 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Using Chromium
-docker run \
+docker run ---rm i \
     -v `pwd`/reports:/opt/robotframework/reports:Z \
     -v `pwd`/test:/opt/robotframework/tests:Z \
     -e BROWSER=chrome \
