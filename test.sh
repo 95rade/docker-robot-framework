20a04531cdf9074f34a33126a52cc9af911e1623
rmilij001c <rade95@gmail.com>
2021-10-29 10:03:07 -0600

    run jdriven tests from suite/

--- test.sh
+++ test.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
-set -ex
-echo "Hello-world"
+#!/usr/bin/env bash
+set -e
+
 pwd
 ls -ltr
 which robot
-robot test/simpletest.robot
+
+# Run an individual test suite if the TEST_SUITE environmental variable is set.
+if [ -z "$TEST_SUITE" ]; then
+    TEST_SUITE=""
+fi
+
+CMD="robot --console verbose --outputdir -d /reports -s /suites/$TEST_SUITE"
+
+echo ${CMD}
+
+``${CMD}``
